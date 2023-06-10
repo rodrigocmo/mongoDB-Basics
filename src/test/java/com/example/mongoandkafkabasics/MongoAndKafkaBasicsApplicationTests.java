af23478f3498010a9a706ba60fa7a93436cd8dd8
Rodrigo Morais <rcastromorais01@gmail.com>
2023-06-10 14:12:31 -0300

    First Commit

---
+++ src/test/java/com/example/mongoandkafkabasics/MongoAndKafkaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongoandkafkabasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoAndKafkaBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
