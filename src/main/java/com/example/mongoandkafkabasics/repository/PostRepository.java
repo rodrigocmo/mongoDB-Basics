5e5c0d1bd76c28ebbfdc89b73c4dfd4ae9658068
Rodrigo Morais <rcastromorais01@gmail.com>
2023-07-20 20:08:25 -0300

    update:findTextInBody,TitleOrComments

--- src/main/java/com/example/mongoandkafkabasics/repository/PostRepository.java
+++ src/main/java/com/example/mongoandkafkabasics/repository/PostRepository.java
@@ -6,12 +6,16 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
     List<Post> findByTitleContaining(String title);
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-    // check de doc to understand how to use this query 
+    // check de doc to understand how to use this query
     List<Post> findByTitle(String title);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
