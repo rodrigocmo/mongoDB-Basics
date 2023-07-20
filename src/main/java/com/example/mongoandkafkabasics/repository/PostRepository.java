952b7befa3b31e512bfdc81a917a605baa1e3ed4
Rodrigo Morais <rcastromorais01@gmail.com>
2023-07-20 19:54:04 -0300

    update: query with regex

--- src/main/java/com/example/mongoandkafkabasics/repository/PostRepository.java
+++ src/main/java/com/example/mongoandkafkabasics/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.example.mongoandkafkabasics.repository;
 import com.example.mongoandkafkabasics.db.Post;
 import com.example.mongoandkafkabasics.db.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
-    List<Post> findByTitleContaining(String name);
+    List<Post> findByTitleContaining(String title);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    // check de doc to understand how to use this query 
+    List<Post> findByTitle(String title);
 }
